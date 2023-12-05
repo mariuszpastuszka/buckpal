a905e5d4ef6909917f0b2fdab8ee7bc4231cc0ae
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-12-05 13:25:25 +0100

    [add] - exercises added

---
+++ src/main/java/com/mpas/buckpal/application/port/in/package-info.java
@@ -0,0 +1,7 @@
+package com.mpas.buckpal.application.port.in;
+
+/**
+ * Program the use case for retrieving account statements:
+ * - use record as dto
+ * - the controller allows you to specify the time range for which you want to receive a statement
+ */
