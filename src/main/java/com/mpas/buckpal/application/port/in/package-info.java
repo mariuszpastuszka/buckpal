1dd9601172164c6d2f80a36f6a80a3bd60f2190d
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-12-06 08:48:55 +0100

    [add] - exercises added part 2

--- src/main/java/com/mpas/buckpal/application/port/in/package-info.java
+++ src/main/java/com/mpas/buckpal/application/port/in/package-info.java
@@ -1,7 +1,14 @@
 package com.mpas.buckpal.application.port.in;
 
 /**
+ * Exercise 1
  * Program the use case for retrieving account statements:
  * - use record as dto
  * - the controller allows you to specify the time range for which you want to receive a statement
+ */
+
+/**
+ * Exercise 2
+ * Add testcontainers to project
+ * - implement test for datasource with it
  */
